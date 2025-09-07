03a1e5d30ae1e1fb705d7239b06b404b28a19ca4
家炫 刘 <1928099560@qq.com>
2025-09-07 19:37:13 +0800

    修改默认页数量

--- src/main/java/com/example/demo/common/PageRequest.java
+++ src/main/java/com/example/demo/common/PageRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class PageRequest {
     private int currentPage = 1; // 当前页
-    private int pageSize = 10;   // 每页中的记录数
+    private int pageSize = 8;   // 每页中的记录数
 
     public int getOffset() {
         return (currentPage - 1) * pageSize;
